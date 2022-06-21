081f6a7bcfd57e13e240bbb9b68dbf6a14a109c6
Igor S <is@embact.io>
2022-06-21 15:30:07 +0300

    2

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // код начнёт выполняться после загрузки страницы
 // когда окно загрузится, сработает метод window.onLoad
-window.addEventListener('load', function OnWindowLoaded() {
-    // набор кнопок
-    var signs = ['c',
-        '1', '2', '3', '/',
-        '4', '5', '6', '*',
-        '7', '8', '9', '-',
-        '+/-', '0', '.', '+',
-        '=', 
-    ];
+window.addEventListener("load", function OnWindowLoaded() {
+    const input = document.getElementById("mainInput"); 
+    const reset = document.getElementById("reset"); 
+    reset.addEventListener("click", resetCalc);
+    
+
+
+
+    function resetCalc() {
+        console.log();
+    }
 });
