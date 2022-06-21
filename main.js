f4bca04eee30031e5d7dadcb9a102a64c01581a5
Igor S <is@embact.io>
2022-06-21 16:33:13 +0300

    1

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 // код начнёт выполняться после загрузки страницы
 // когда окно загрузится, сработает метод window.onLoad
 window.addEventListener("load", function OnWindowLoaded() {
-    const input = document.getElementById("mainInput"); 
-    const reset = document.getElementById("reset"); 
+    const input = document.getElementById("mainInput");
+    const reset = document.getElementById("reset");
     reset.addEventListener("click", resetCalc);
     
-
-
-
     function resetCalc() {
+        input.value = '0';
         console.log();
     }
 });
